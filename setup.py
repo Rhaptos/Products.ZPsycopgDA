7d00c868bc3957cd7c13a1ac68ea7045c977b3a9
Josh LaPlace <josh@cnx.rice.edu>
2009-09-29 17:32:58 +0000

    add egg structures for thirdparty products

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.11-rhaptos'
+
+setup(name='Products.ZPsycopgDA',
+      version=version,
+      description="",
+      long_description="",
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='',
+      license='',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products', ],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+      ],
+      tests_require = [
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
+
