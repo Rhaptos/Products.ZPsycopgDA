08c8db76f7462cc5e16a629b610bb1f9cc16d16a
Ross J. Reedstrom <reedstrm@cnx.org>
2012-08-27 17:05:06 +0000

    extend allowed versions of psycopg2 library (underlying API seems unchanged)

--- Products/ZPsycopgDA/DA.py
+++ Products/ZPsycopgDA/DA.py
@@ -16,7 +16,7 @@
 # their work without bothering about the module dependencies.
 
 
-ALLOWED_PSYCOPG_VERSIONS = ('2.4-beta1', '2.4-beta2', '2.4', '2.4.1', '2.4.2', '2.4.3', '2.4.4', '2.4.5')
+ALLOWED_PSYCOPG_VERSIONS = ('2.0.7','2.0.8','2.0.9','2.0.10', '2.0.11', '2.0.12', '2.0.13','2.4-beta1', '2.4-beta2', '2.4', '2.4.1', '2.4.2', '2.4.3', '2.4.4', '2.4.5')
 
 import sys
 import time
